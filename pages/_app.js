324f1edb4dc68fea94091d0046230c9688a0dde1
TheHamsterDog <shreshth@shreshthverma.me>
2022-10-29 13:11:32 +0530

    feat: added SEO tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <>
+    <Head> 
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="og:image" content='/og-image.png'/>
+      <meta name="description" content="A safe, supportive and empowering home for the local LGBTQ+ community. We believe in a world where all people are free to express their gender identity and sexual orientation with pride."/>
+      <meta name="og:description" content='A safe, supportive and empowering home for the local LGBTQ+ community. We believe in a world where all people are free to express their gender identity and sexual orientation with pride.'/>
+      <meta name="og:title" content='QueerTeen India: the Safe Space of Desi Queer Teens'/>
+      <meta property="og:locale" content="en_GB" />
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
