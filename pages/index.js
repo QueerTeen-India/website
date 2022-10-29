fb55e493e25dd5cd075961904ac7cde608257a9a
TheHamsterDog <shreshth@shreshthverma.me>
2022-10-30 02:49:27 +0530

    feat: created navbar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>QueerTeen India</title>
-        <meta name="description"/>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
-}
+import Home from '../components/Home';
+export default Home;
