8cc6a07eea4a5e7fd26f7f6d102d78877837270d
TheHamsterDog <shreshth@shreshthverma.me>
2023-04-11 19:34:28 +0530

    feat: added news

---
+++ components/news/index.jsx
@@ -0,0 +1,143 @@
+const News = () => {
+    const news = [
+        {
+            title: 'Governor Ron DeSantis comes out of the closet, addresses internal homophobia',
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat lacus laoreet non curabitur. .",
+            img: "https://www.gannett-cdn.com/presto/2020/11/13/NLED/828733e3-ce00-4f19-acc0-fa8859743beb-USATSI_15186033.jpg?width=660&height=440&fit=crop&format=pjpg&auto=webp",
+            date: 'April 11th, 2023',
+            article: `
+        # Ron DeSantis is Gay
+        ## In a shocking turn of events, the homophobic fucktard reveals himself as gay  
+        
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat lacus laoreet non curabitur. Nulla aliquet porttitor lacus luctus. Nibh cras pulvinar mattis nunc. Mauris vitae ultricies leo integer malesuada. Eros donec ac odio tempor orci dapibus ultrices in. At imperdiet dui accumsan sit amet nulla facilisi. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Faucibus nisl tincidunt eget nullam non nisi. Sagittis nisl rhoncus mattis rhoncus urna. Urna molestie at elementum eu. Enim sed faucibus turpis in eu mi. Risus nullam eget felis eget nunc lobortis mattis.
+        
+            {
+            
+                "name":  "queerteen-india",
+            
+                "version":  "0.1.0",
+            
+                "private":  true,
+            
+                "scripts": {
+            
+                    "dev":  "next dev",
+            
+                    "build":  "next build",
+            
+                    "start":  "next start",
+            
+                    "style":  "sass ./sass/main.scss ./styles/globals.css -w",
+            
+                    "lint":  "next lint"
+            
+            },
+            "dependencies": {
+            
+                "@emotion/react":  "^11.10.6",
+            
+                "@emotion/styled":  "^11.10.6",
+            
+                "@mui/icons-material":  "^5.11.16",
+                
+                "@mui/material":  "^5.11.16",
+                
+                "concurrently":  "^7.6.0",
+                
+                "next":  "^13.0.0",
+                
+                "object-assign":  "^4.1.1",
+                
+                "prop-types":  "^15.8.1",
+                
+                "react":  "^18.2.0",
+                
+                "react-dom":  "^18.2.0",
+                
+                "react-router-dom":  "^6.10.0",
+                
+                "sass":  "^1.60.0"
+            
+            },
+            "devDependencies": {
+                
+                "eslint":  "8.26.0",
+                
+                "eslint-config-next":  "13.0.0"
+            }    
+            }
+            
+                
+        
+        In nibh mauris cursus mattis molestie a iaculis at erat. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. Accumsan lacus vel facilisis volutpat. Risus pretium quam vulputate dignissim suspendisse in. Risus pretium quam vulputate dignissim suspendisse in est ante in. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Donec ultrices tincidunt arcu non sodales neque sodales ut etiam. Ut tortor pretium viverra suspendisse potenti nullam. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Pulvinar elementum integer enim neque volutpat ac. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Imperdiet sed euismod nisi porta lorem mollis aliquam ut. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Nec sagittis aliquam malesuada bibendum. Nunc mattis enim ut tellus elementum sagittis vitae et. Venenatis lectus magna fringilla urna. Lectus quam id leo in vitae. Fermentum dui faucibus in ornare quam viverra orci sagittis.
+        
+         - Lorem
+         - Ipsum
+         - Nibh
+         - Mauris
+         - Cursus
+        
+        
+        Diam quis enim lobortis scelerisque fermentum dui faucibus in. Suscipit adipiscing bibendum est ultricies integer quis auctor elit. Ultrices dui sapien eget mi proin sed libero enim sed. Pellentesque adipiscing commodo elit at imperdiet dui accumsan. In hac habitasse platea dictumst vestibulum rhoncus est. Cras pulvinar mattis nunc sed blandit libero volutpat. Consectetur adipiscing elit pellentesque habitant morbi. Dui ut ornare lectus sit. Diam quam nulla porttitor massa id neque. Viverra nam libero justo laoreet sit amet cursus sit amet. Hac habitasse platea dictumst vestibulum rhoncus est. Pellentesque dignissim enim sit amet venenatis urna cursus eget nunc. Augue lacus viverra vitae congue eu consequat ac felis donec.
+        
+        ![Ron DeSantis being Gay](https://storage.googleapis.com/afs-prod/media/9d06201efd2949ed9e1d95f76cbb0da8/3000.jpeg)
+        
+        Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Vel fringilla est ullamcorper eget nulla facilisi etiam. Fusce id velit ut tortor pretium. At erat pellentesque adipiscing commodo elit at. Neque convallis a cras semper auctor neque vitae tempus quam. Nulla pellentesque dignissim enim sit amet venenatis. Neque gravida in fermentum et. Pellentesque dignissim enim sit amet venenatis urna cursus eget. Est velit egestas dui id ornare. Eget nunc lobortis mattis aliquam. Aliquet nibh praesent tristique magna sit amet purus gravida quis. Neque gravida in fermentum et sollicitudin. Id volutpat lacus laoreet non curabitur gravida arcu. Arcu dui vivamus arcu felis bibendum.
+        
+        Elementum tempus egestas sed sed risus pretium quam. Ullamcorper morbi tincidunt ornare massa. Congue quisque egestas diam in arcu. Ac odio tempor orci dapibus ultrices in iaculis nunc. Eu consequat ac felis donec et odio. Neque vitae tempus quam pellentesque nec nam aliquam sem. Amet aliquam id diam maecenas. Cras fermentum odio eu feugiat pretium nibh ipsum consequat. Commodo viverra maecenas accumsan lacus vel facilisis. Non arcu risus quis varius. Pharetra convallis posuere morbi leo urna.
+        
+        > LOREM IPSUM EGESTAS SED SED RISUS
+        
+        `
+        }]
+    return (
+        <div className="news">
+            <div className='news-header'>
+                <h1>News</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+            </div>
+            <div className="news-container">
+                {news.map((newsItem, index) => (
+                    <div key={index} style={{ backgroundImage: `linear-gradient(to right bottom, #00000080, #000), url(${newsItem.img})` }} className="news-container-item">
+                        <h2>{newsItem.title}</h2>
+                        <p>{newsItem.description}</p>
+                    </div>
+                ))}
+                {news.map((newsItem, index) => (
+                    <div key={index} style={{ backgroundImage: `linear-gradient(to right bottom, #00000080, #000), url(${newsItem.img})` }} className="news-container-item">
+                        <h2>{newsItem.title}</h2>
+                        <p>{newsItem.description}</p>
+                    </div>
+                ))}
+                {news.map((newsItem, index) => (
+                    <div key={index} style={{ backgroundImage: `linear-gradient(to right bottom, #00000080, #000), url(${newsItem.img})` }} className="news-container-item">
+                        <h2>{newsItem.title}</h2>
+                        <p>{newsItem.description}</p>
+                    </div>
+                ))}
+                {news.map((newsItem, index) => (
+                    <div key={index} style={{ backgroundImage: `linear-gradient(to right bottom, #00000080, #000), url(${newsItem.img})` }} className="news-container-item">
+                        <h2>{newsItem.title}</h2>
+                        <p>{newsItem.description}</p>
+                    </div>
+                ))}
+                {news.map((newsItem, index) => (
+                    <div key={index} style={{ backgroundImage: `linear-gradient(to right bottom, #00000080, #000), url(${newsItem.img})` }} className="news-container-item">
+                        <h2>{newsItem.title}</h2>
+                        <p>{newsItem.description}</p>
+                    </div>
+                ))}
+                {news.map((newsItem, index) => (
+                    <div key={index} style={{ backgroundImage: `linear-gradient(to right bottom, #00000080, #000), url(${newsItem.img})` }} className="news-container-item">
+                        <h2>{newsItem.title}</h2>
+                        <p>{newsItem.description}</p>
+                    </div>
+                ))}
+
+            </div>
+        </div>
+    )
+}
+
+export default News;
